0fc9ec4a40d4dea8a7ce5cef0d8ecb138a373ea8
Júlia Vicentini <126635565+juvicentini@users.noreply.github.com>
2024-09-30 00:27:08 +0000

    estudoProva

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule]
 })
+
 export class HomePage {
-  constructor() {}
+  constructor() { }
+  public gasolina: number | undefined;
+  public alcool: number | undefined;
+  public proporcao: number | undefined;
+  public situacao: string = "";
+  public porcentagem: string = "";
+  calcularBestOpcao() {
+    if (this.gasolina && this.alcool) {
+      this.proporcao = (this.alcool / this.gasolina);
+      this.porcentagem = (this.proporcao * 100 + '%');
+      this.situacao = this.proporcao <= 0.7 ? 'Álcool' : 'Gasolina';
+    }
+  }
 }
